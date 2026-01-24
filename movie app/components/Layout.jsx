773f6c210443deca476eb1a0f75b97c108854322
jeremytey <jeremy8598@gmail.com>
2026-01-25 01:07:42 +0800

    updated

---
+++ movie app/components/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Link } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/watchlist">Watchlist</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
--- movie app/src/App.jsx
+++ movie app/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Layout from '/components/Layout';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
